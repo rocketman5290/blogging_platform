1fc2698b8fa11a174bad401f102f083a20d5c782
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-10-24 09:36:25 -0400

    Add: Navbar, Loader, and routes to /pages/...

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
-
+import Navbar from '../components/Navbar'
 import { GetServerSideProps } from 'next/app';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+   
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+import Loader from '../components/Loader.js';
+
 
 export default function Home() {
   return (
     <div >
-      
+      <Loader show />
+      <Link prefetch={false} href={{
+        pathname: '/[username]',
+        query: { username: 'rocketman529' },
+      }}>
+        <a>Rocketman529&apos;s profile</a>
+      </Link>
+       
     </div>
   )
 }
