e3e88805fe71b2de7c45fdfc8f983445ed9e8095
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-10-26 09:43:22 -0400

    UpdateLogin: Google signin works, added logo.

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,47 @@
-export default function EnterPage({  }) {
+import { auth, googleAuthProvider } from '../lib/firebase';
+import { signInWithPopup } from 'firebase/auth'
+import Image from 'next/image'
+
+export default function Enter(props) {
+    const user = null;
+    const username = null;
+
     return (
         <main>
+            { user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton /> }
             <h1>Sign up</h1>
        </main>
     )
+}
+
+//Sign in with google button
+// Sign in with Google button
+function SignInButton() {
+    const signInWithGoogle = async () => {
+      await signInWithPopup(auth, googleAuthProvider)
+    };
+  
+    return (
+      <>
+        <button className="btn-google" onClick={signInWithGoogle}>
+          <img src={'/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png'} />Sign in with Google
+        </button>
+        <button onClick={() => signInAnonymously(auth)}>
+          Sign in Anonymously
+        </button>
+      </>
+    );
+  }
+
+//Sign out button
+function SignOutButton() {
+    <button>SignOut Button</button>
+}
+
+//Username form
+function UsernameForm() {
+    <form>
+        <label>username:</label>
+        <input type="" value=""></input>
+    </form>
 }
