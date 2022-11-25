eb9368ed3eedc317b33586f62d6280cde61a6f02
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-11-25 13:48:54 -0500

    Update: pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,33 @@ import '../styles/globals.css';
 
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
-import { UserContext } from '../lib/context'
+import { UserContext } from '../lib/context';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function MyApp({ Component, pageProps }) {
+  // const [user] = useAuthState(auth);
+  // const [username, setUsername] = useState(null);
+
+  // useEffect(() => {
+  //   // turn off realtime subscription from firebase
+  //   let unsubscribe;
+
+  //   if (user) {
+  //     const ref = firestore.collection('users').doc(user.uid)
+  //     unsubscribe = ref.onSnapshot((doc) => {
+  //       setUsername(doc.data()?.username);
+  //     });
+  //   } else {
+  //     setUsername(null);
+  //   }
+
+  //   return unsubscribe;
+  // }, [user])
+
   return (
     <>
-      <UserContext.Provider value={{user: {}, username: "williamgeorge"}} >
+      <UserContext.Provider value={{user: null, username: "williamgeorge"}} >
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
